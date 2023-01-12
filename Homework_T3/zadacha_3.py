802ac4b17a49e401d9de48779b8a5924c6d71e8b
Dmitri <dmitri270799@gmail.com>
2023-01-12 13:14:59 +0300

    изправление ошибок

--- Homework_T3/zadacha_3.py
+++ Homework_T3/zadacha_3.py
@@ -1,20 +1,21 @@
 name = (input("Введите ваше имя: "))
 age = (input("Введите ваш возраст: "))
 while 2 == 2:
-    if age.isdigit():
-        if int(age) <= 0:
+    if age.isdigit() and name.isalpha():
+        age = int(age)
+        if age <= 0:
             print("Ошибка, повторите ввод")
             name = (input("Введите ваше имя ещё раз: "))
             age = (input("Введите ваш возраст ещё раз: "))
-        elif int(age) > 0 and int(age) < 10:
+        elif age > 0 and age < 10:
             print(f"Привет, шкет {name}")
             name = (input("Введите ваше имя ещё раз: "))
             age = (input("Введите ваш возраст ещё раз: "))
-        elif int(age) >= 10 and int(age) <= 18:
+        elif age >= 10 and age <= 18:
             print(f"Как жизнь {name}?")
             name = (input("Введите ваше имя ещё раз: "))
             age = (input("Введите ваш возраст ещё раз: "))
-        elif int(age) > 18 and int(age) < 100:
+        elif age > 18 and age < 100:
             print(f"Что желаете {name}?")
             name = (input("Введите ваше имя ещё раз: "))
             age = (input("Введите ваш возраст ещё раз: "))
