d12f70592d0db7572cb75e7a2c40018f25634ea5
Dmitri <dmitri270799@gmail.com>
2023-01-18 13:31:37 +0300

    second_commit

--- Homework_T4/zadacha_4.py
+++ Homework_T4/zadacha_4.py
@@ -2,12 +2,9 @@ from datetime import datetime
 import time
 
 
-def time_click(n):
-    new_list = []
-    for i in range(n):
-        new_list.append([datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')])
-        time.sleep(1)
-    print(new_list)
+def time_click():
+    time.sleep(1)
+    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 
 
-time_click(int(input("Введите кол-во элементов нового списка: ")))
+print([time_click() for i in range(int(input("Введите кол-во элементов нового списка: ")))])
